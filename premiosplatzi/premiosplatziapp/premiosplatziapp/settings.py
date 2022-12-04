6308835bae46fab8a277d4ba88dbc6ef5d8e59e8
unmecaniko-wsl <velasquezlopezn@gmail.com>
2022-12-04 15:16:12 -0500

    update settings

--- premiosplatzi/premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatzi/premiosplatziapp/premiosplatziapp/settings.py
@@ -33,14 +33,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
